ee56e8b154ae8004ef5bee40287d657a831a2c4d
Almeidiano <sanuel10ap@gmail.com>
2023-10-06 15:09:05 -0300

    First steps: upon current chapter, toggle like added

--- src/app/Components/icons/LikeButton.tsx
+++ src/app/Components/icons/LikeButton.tsx
@@ -1,9 +1,23 @@
-import { GoHeart } from "react-icons/go";
+"use client"
+
+import { useState } from "react";
+import { GoHeart, GoHeartFill } from "react-icons/go";
 
 export const LikeButton = ({ size }: { size?: string }) => {
+  const [like, setLike] = useState(false)
+
+  function toggleLike() {
+    setLike(!like)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
-      <GoHeart className={size}></GoHeart>
+      {
+        like ? 
+        <GoHeartFill onClick={toggleLike} className={`${size} cursor-pointer text-red-500`}></GoHeartFill>
+        :
+        <GoHeart onClick={toggleLike} className={`${size} cursor-pointer`}></GoHeart>
+      }
       <p className="text-xs">472</p>
     </div>
   );
